b2f7f35765a7fcd436b78de038d8af898afea737
Lauren Matos <laurenamariah@Tatianas-Air.fios-router.home>
2024-10-24 13:30:46 -0400

    First commit

--- SQL Alchemy challege/app.py
+++ SQL Alchemy challege/app.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
-
+import datetime as dt
 
 #################################################
 # Database Setup
@@ -37,49 +37,57 @@ app = Flask(__name__)
 #################################################
 @app.route('/')
 def home():
-    return jsonify({
-        "Available Routes": [
-            "/api/v1.0/precipitation",
-            "/api/v1.0/stations",
-            "/api/v1.0/tobs",
-            "/api/v1.0/<start>",
-            "/api/v1.0/<start>/<end>"
-        ]
-    })
+    return(
+        f"Available Routes:<br/>"
+            f"/api/v1.0/precipitation<br/>"
+            f"/api/v1.0/stations<br/>"
+            f"/api/v1.0/tobs<br/>"
+            f"/api/v1.0/<start><br/>"
+            f"/api/v1.0/<start>/<end><br/>"
+            )
 
 @app.route('/api/v1.0/precipitation')
 def precipitation():
-    # Query to get precipitation data for the last year
-    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= one_year).all()
+    one_year = dt.date(2017,8,23)- dt.timedelta(days = 365)
+    results = session.query(measurement.date, measurement.prcp).filter(measurement.date >= one_year).all()
+    session.close()
     precipitation_data = {date: prcp for date, prcp in results}
     return jsonify(precipitation_data)
 
 @app.route('/api/v1.0/stations')
 def stations():
-    results = session.query(Station.station).all()
+    results = session.query(station.station).all()
+    session.close()
     stations_list = [station[0] for station in results]
     return jsonify(stations_list)
 
 @app.route('/api/v1.0/tobs')
 def tobs():
-    results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.station == 'USC00519281', Measurement.date >= one_year_ago).all()
+    one_year = dt.date(2017,8,23)- dt.timedelta(days = 365)
+    results = session.query(measurement.date, measurement.tobs).filter(measurement.station == 'USC00519281', measurement.date >= one_year).all()
+    session.close()
     tobs_list = [{date: tobs} for date, tobs in results]
     return jsonify(tobs_list)
 
 @app.route('/api/v1.0/<start>')
 def start(start):
-    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).all()
-    return jsonify({
-        "TMIN": results[0][0],
-        "TAVG": results[0][1],
-        "TMAX": results[0][2]
-
-
-@app.route('/api/v1.0/<start>/<end>')
-def start_end(start, end):
-    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    results = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).filter(measurement.date >= start).all()
+    session.close()
     return jsonify({
         "TMIN": results[0][0],
         "TAVG": results[0][1],
         "TMAX": results[0][2]
     })
+
+@app.route('/api/v1.0/<start>/<end>')
+def start_end(start, end):
+    results = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).filter(measurement.date >= start).filter(measurement.date <= end).all()
+    session.close()
+    return jsonify({
+        "TMIN": results[0][0],
+        "TAVG": results[0][1],
+        "TMAX": results[0][2]
+    })
+
+if __name__ == "__main__":
+    app.run()
